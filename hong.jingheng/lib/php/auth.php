036ec88ec5fb0bd2b99053c50ad2c7e5d9849206
alicehongjingheng <alice.jingheng@gmail.com>
2025-11-12 19:31:25 +0800

    Add files via upload

---
+++ hong.jingheng/lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth(){
+	return [
+		"localhost",  // mysql host
+		"bigheadbing",  // mysql username
+		"bigheadbing0102!",  // mysql user password
+		"bigheadbing_wm608"  // mysql database name
+	];
+}
