e12ad6e1493bd5bc0f8af5384b0111dc74b07a0a
alicehongjingheng <alice.jingheng@gmail.com>
2025-12-10 12:40:28 +0800

    Module 14 Update

--- hong.jingheng/lib/php/auth.php
+++ hong.jingheng/lib/php/auth.php
@@ -8,3 +8,12 @@ function MYSQLIAuth(){
 		"bigheadbing_wm608"  // mysql database name
 	];
 }
+
+function PDOAuth(){
+	return [
+		"mysql:host=localhost;dbname=bigheadbing_wm608",  // host and database name 
+		"bigheadbing",  // mysql username
+		"bigheadbing0102!",  // mysql user password
+		[PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"]
+	];
+}
